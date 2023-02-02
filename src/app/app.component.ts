e35c41fa577342dc31e3987da980ce788915a170
Felix Bauer <kontakt@felix-bauer.at>
2023-02-02 21:00:08 +0100

    Use LESS, first basic layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.less']
 })
 export class AppComponent {
   title = 'static-web-app';
+  footerCodes: string[] = [
+    "model.FindAnnotation(SqlServerAnnotationNames.HiLoSequenceName)?.GetConfigurationSource();",
+    "Assert.notNull(this.method, () -> \"Could not obtain target method from JoinPoint: '\" + jp + \"'\");",
+    "<p>Your competent partner</p>",
+    "\"launchUrl\": \"{Scheme}://{ServiceHost}:{ServicePort}\",",
+    "powershell.exe -ExecutionPolicy ByPass ./msi/BuildInstaller.ps1"
+  ]
+
+  public openContactForm() {
+    console.log("asd");
+  }
 }
