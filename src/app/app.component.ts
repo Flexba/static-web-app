a426003f64f2d286b1081c51ffe4d63f28f76ac7
Felix Bauer <kontakt@felix-bauer.at>
2023-02-14 23:13:05 +0100

    Added footer & general changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,16 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'static-web-app';
-  footerCodes: string[] = [
-    "model.FindAnnotation(SqlServerAnnotationNames.HiLoSequenceName)?.GetConfigurationSource();",
-    "Assert.notNull(this.method, () -> \"Could not obtain target method from JoinPoint: '\" + jp + \"'\");",
-    "<p>Your competent partner</p>",
-    "\"launchUrl\": \"{Scheme}://{ServiceHost}:{ServicePort}\",",
-    "powershell.exe -ExecutionPolicy ByPass ./msi/BuildInstaller.ps1"
-  ]
-
-  public openContactForm() {
-    console.log("asd");
-  }
+  visible = false;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
-
 import { AppComponent } from './app.component';
 import { TeamComponent } from './team/team.component';
 import { ContactFormComponent } from './contact-form/contact-form.component';
+import { ServicesComponent } from './services/services.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TeamComponent,
-    ContactFormComponent
+    ContactFormComponent,
+    ServicesComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
